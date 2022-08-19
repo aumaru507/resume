f3f4a97a3df69dc7b8856302a37d2f4bdb03d789
aumaru <setthanan507@gmail.com>
2022-08-19 11:14:05 +0700

    update Skills

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,17 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-    myScriptElement: HTMLScriptElement;
-    myScriptElement1: HTMLScriptElement;
-
   constructor() {
-    this.myScriptElement = document.createElement("script");
-    this.myScriptElement.src="src/assets/js/main.js";
-    document.body.appendChild(this.myScriptElement);
-
-    this.myScriptElement1 = document.createElement("script");
-    this.myScriptElement1.src="src/assets/js/swiper-bundle.min.js";
-    document.body.appendChild(this.myScriptElement1);
   }
 
   ngOnInit(): void {
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,23 +13,24 @@
 
 
 /*==================== ACCORDION SKILLS ====================*/
-
+function hello(){
 const skillsContent = document.getElementsByClassName('skills_content'),
-      skillsHeader =  document.querySelectorAll('.skills_header')
+      skillsHeader = document.querySelectorAll('.skills_header')
 
-function toggleSkills(){
-  let itemClass = this.parentNode.className
-  for(i = 0; i < skillsContent.length; i++){
-    skillsContent[i].className = 'skills_content skills_close'
-}
-  if(itemClass === 'skills_content skills_close'){
-    this.parentNode.className = 'skills_content skills_open'
-}
-}
-skillsHeader.foreach((el) =>{
-  el.addEventListener('click', toggleSkills)
-})
+      function toggleSkills(){
+        let itemClass = this.parentNode.className
 
+        for(i = 0; i < skillsContent.length; i++){
+          skillsContent[i].className = 'skills_content skills_close'
+        }
+        if(itemClass === 'skills_content skills_close'){
+          this.parentNode.className = 'skills_content skills_open'
+        }
+      }
+      skillsHeader.forEach((el)=>{
+        el.addEventListener('click',toggleSkills)
+      })
+}
 /*==================== QUALIFICATION TABS ====================*/
 
 
