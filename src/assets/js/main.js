7d652931cc1d9be17d40e9c75fadb777949e5c6b
aumaru <setthanan507@gmail.com>
2022-08-24 15:09:48 +0700

    update home.component

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,55 +1,10 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
 
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
-
-
-/*==================== ACCORDION SKILLS ====================*/
-function hello(){
-const skillsContent = document.getElementsByClassName('skills_content'),
-      skillsHeader = document.querySelectorAll('.skills_header')
-
-      function toggleSkills(){
-        let itemClass = this.parentNode.className
-
-        for(i = 0; i < skillsContent.length; i++){
-          skillsContent[i].className = 'skills_content skills_close'
-        }
-        if(itemClass === 'skills_content skills_close'){
-          this.parentNode.className = 'skills_content skills_open'
-        }
-      }
-      skillsHeader.forEach((el)=>{
-        el.addEventListener('click',toggleSkills)
-      })
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("navbar").style.background = "#0A6C8B";
+  } else {
+    document.getElementById("navbar").style.background = "none";
+  }
 }
-/*==================== QUALIFICATION TABS ====================*/
 
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/
-
-
-/*==================== SHOW SCROLL UP ====================*/
-
-
-/*==================== DARK LIGHT THEME ====================*/
